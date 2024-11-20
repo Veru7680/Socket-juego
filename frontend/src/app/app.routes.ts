46a525ea3aaeaf3f58f2b240ffe96c716c3b1819
Veru7680 <veru6198@gmail.com>
2024-11-19 23:18:03 -0400

    se crear carpeta frontend y home, room

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RoomComponent } from './room/room.component';
+
+export const routes: Routes = [
+    { path: '', 
+        component: HomeComponent },
+    { path: ':room', 
+        component: RoomComponent } 
+];
+
+
+
